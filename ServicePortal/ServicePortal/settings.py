424ca12b413fc866a4a3df71051f0308f22f6fb0
bubbaayala <2015.ayalal@gmail.com>
2018-07-05 07:48:53 -0700

    Update settings.py

--- ServicePortal/ServicePortal/settings.py
+++ ServicePortal/ServicePortal/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '361mtr5&u(y1us+xbbygcs=0di5ijwk$%^z#ts=mg7zqlpy1j^'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
